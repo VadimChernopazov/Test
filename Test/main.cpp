0daed76cb78fa30019fd23b5c2603fdd9091a348
VadimChernopazov <freelimelectronic@gmail.com>
2020-08-02 18:59:13 +0500

    First commit is created.

---
+++ Test/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib> // Для работы с функцией system()
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdint.h>
+
+
+int main(int argc, char** argv) {
+	uint8_t tmp=8;
+	
+	for(;tmp<15;tmp++){
+	printf("Hello world!  %d\n",tmp);	
+	
+	}
+	
+	system("pause"); // Команда задержки экрана
+	return 0;
+}
